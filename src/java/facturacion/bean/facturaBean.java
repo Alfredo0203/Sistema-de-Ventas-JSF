20507dbeae751fe4f50a6da811d673ae3655ecc2
Alfredo Torres <alfredotorres.niu@gmail.com>
2021-02-10 16:50:51 -0600

    Se implementó método para buscar usuario por id

--- src/java/facturacion/bean/facturaBean.java
+++ src/java/facturacion/bean/facturaBean.java
@@ -28,7 +28,7 @@ public class facturaBean {
     Transaction transaction = null;
 
     private Cliente cliente;
-    private String codigoCliente;
+    private Integer codigoCliente;
     public facturaBean() {
     }
 
@@ -40,11 +40,11 @@ public class facturaBean {
         this.cliente = cliente;
     }
 
-    public String getCodigoCliente() {
+    public Integer getCodigoCliente() {
         return codigoCliente;
     }
 
-    public void setCodigoCliente(String codigoCliente) {
+    public void setCodigoCliente(Integer codigoCliente) {
         this.codigoCliente = codigoCliente;
     }
     
@@ -63,6 +63,54 @@ public class facturaBean {
             this.cliente = cdao.obtenerClientePorCodigo(this.session, codCliente);
             
             this.transaction.commit();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Correcto","Datos cliente agregado"));
+                    
+        } catch (Exception e) {
+           
+            if (this.transaction != null) {
+                System.out.println(e.getMessage());
+                transaction.rollback();
+                
+            }
+            
+        }finally {
+            if (this.session != null) {
+             this.session.close();
+            }
+        }
+    }
+    
+     //Metodo para mostrar los datos de clientes mediante codigo
+    
+    public void agregarDatosClientes2() {
+        this.session = null;
+        this.transaction = null;
+        
+        try {
+            
+            if (this.codigoCliente == null) {
+                return;
+            }
+            this.session = HibernateUtil.getSessionFactory().openSession();
+            clienteDao cdao = new clienteDaoImp();
+            this.transaction = this.session.beginTransaction();
+            
+            // Guardar los datos en la variable objeto cliente segun el codigo
+            this.cliente = cdao.obtenerClientePorCodigo(this.session, this.codigoCliente);
+            
+            if (this.cliente != null) {
+                this.codigoCliente = null;
+                 FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Correcto","Datos cliente agregado"));
+             
+            }
+            else {
+                this.codigoCliente = null;
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error","No se encontró cliente"));
+               
+            }
+            
+            this.transaction.commit();
+            
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Correcto","Datos cliente agregado"));
                     
         } catch (Exception e) {
